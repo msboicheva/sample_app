29169728b4ec913f111d23031e91f5ed5ae61b74
sboicheva <silvia.boicheva@shtrak.bg>
2014-03-06 13:00:01 +0200

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,47 @@ describe "Static pages" do
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
-  end
-
-  describe "Help page" do
-    before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
-  end
-
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
-  end
-
-  describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+   it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    expect(page).to have_title(full_title('| Home'))
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up now!'))
+    click_link "sample app"
+    expect(page).to have_title(full_title('sample app'))
   end
 end
+
+# describe "Home page" do
+  #   before { visit root_path }
+
+  #   it { should have_content('Sample App') }
+  #   it { should have_title(full_title('')) }
+  #   it { should_not have_title('| Home') }
+  # end
+
+  # describe "Help page" do
+  #   before { visit help_path }
+
+  #   it { should have_content('Help') }
+  #   it { should have_title(full_title('Help')) }
+  # end
+
+  # describe "About page" do
+  #   before { visit about_path }
+
+  #   it { should have_content('About') }
+  #   it { should have_title(full_title('About Us')) }
+  # end
+
+  # describe "Contact page" do
+  #   before { visit contact_path }
+
+  #   it { should have_content('Contact') }
+  #   it { should have_title(full_title('Contact')) }
